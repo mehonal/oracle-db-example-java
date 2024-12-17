4721a11bfbaf9439a40510d3eea632c96f8fedbc
Mehonal <116497904+mehonal@users.noreply.github.com>
2024-12-17 11:27:21 +0300

    added connection to Oracle DB, inserting book records

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,53 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static Properties props = new Properties();
+
+    static {
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Unable to find application.properties");
+            }
+            props.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading application.properties", e);
         }
     }
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            try (Connection conn = DriverManager.getConnection(
+                    props.getProperty("db.url"),
+                    props.getProperty("db.username"),
+                    props.getProperty("db.password"))) {
+
+                String insertSQL = "INSERT INTO BOOK (ID, NAME, ISBN, CREATE_DATE) VALUES (SEQ_BOOK_ID.NEXTVAL, ?, ?, SYSDATE)";
+
+                try (PreparedStatement pstmt = conn.prepareStatement(insertSQL)) {
+
+                    for (int i = 0; i < 100; i++) {
+                        String bookName = "Book Amazing Adventures of Java";
+                        String isbn = "123-456-789";
+                        pstmt.setString(1, bookName);
+                        pstmt.setString(2, isbn);
+                        pstmt.executeUpdate();
+
+                        System.out.println("Inserted book: " + bookName + " with ISBN: " + isbn);
+                    }
+                    System.out.println("Successfully inserted 100 records!");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
